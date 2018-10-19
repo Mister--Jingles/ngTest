a2aec1be47b6b8deaa7876fc319289635f22ea00
v.r.kostenko <v.r.kostenko@tinkoff.ru>
2018-10-19 16:14:25 +0300

    * add local storage test

--- src/app/rxjs/async.component.spec.ts
+++ src/app/rxjs/async.component.spec.ts
@@ -6,112 +6,60 @@ import {AsyncService} from './async.service';
 import {of} from 'rxjs';
 
 describe(`${AsyncComponent.name} | Component with async service:`, () => {
-  describe('real service instance:', () => {
-    // region Test Setup
-    let component: AsyncComponent;
-    let fixture: ComponentFixture<AsyncComponent>;
-    let serviceMock: DependentService;
+  // region Test Setup
+  let component: AsyncComponent;
+  let fixture: ComponentFixture<AsyncComponent>;
+  let serviceMock: DependentService;
 
-    beforeEach(() => {
-      serviceMock = mock<DependentService>(DependentService);
-    });
-
-    beforeEach(() => {
-      fixture = TestBed
-        .configureTestingModule({
-          declarations: [AsyncComponent],
-          providers: [
-            AsyncService,
-            {provide: DependentService, useValue: instance(serviceMock)}
-          ]
-        })
-        .overrideTemplate(AsyncComponent, '')
-        .createComponent(AsyncComponent);
-
-      component = fixture.componentInstance;
-
-      fixture.detectChanges();
-    });
-    // endregion
-
-    describe('timer:', () => {
-      it('[sync]: should call callDoSmthInteresting method', () => {
-        component.callTimer();
-
-        verify(serviceMock.callDoSmthInteresting()).never();
-      });
-
-      it('[fakeAsync]: should call callDoSmthInteresting method', fakeAsync(() => {
-        component.callTimer();
-        tick(AsyncService.TIME);
-
-        verify(serviceMock.callDoSmthInteresting()).once();
-      }));
-    });
-
-    describe('debounce:', () => {
-      it('[sync]: should call callDoSmthInteresting method', () => {
-        component.callDebounce();
-
-        verify(serviceMock.callDoSmthInteresting()).never();
-      });
-
-      it('[fakeAsync]: should call callDoSmthInteresting method', fakeAsync(() => {
-        component.callDebounce();
-        tick(AsyncService.TIME);
-
-        verify(serviceMock.callDoSmthInteresting()).once();
-      }));
-    });
+  beforeEach(() => {
+    serviceMock = mock<DependentService>(DependentService);
   });
 
-  describe('mocked service:', () => {
-    // region Test Setup
-    let component: AsyncComponent;
-    let fixture: ComponentFixture<AsyncComponent>;
-    let serviceMock: DependentService;
-    let asyncServiceMock: AsyncService;
+  beforeEach(() => {
+    fixture = TestBed
+      .configureTestingModule({
+        declarations: [AsyncComponent],
+        providers: [
+          AsyncService,
+          {provide: DependentService, useValue: instance(serviceMock)}
+        ]
+      })
+      .overrideTemplate(AsyncComponent, '')
+      .createComponent(AsyncComponent);
 
-    beforeEach(() => {
-      serviceMock = mock<DependentService>(DependentService);
-      asyncServiceMock = mock<AsyncService>(AsyncService);
+    component = fixture.componentInstance;
 
-      when(asyncServiceMock.debounce$()).thenReturn(of(null));
-      when(asyncServiceMock.timer$()).thenReturn(of(null));
+    fixture.detectChanges();
+  });
+  // endregion
+
+  describe('timer:', () => {
+    it('[sync]: should call callDoSmthInteresting method', () => {
+      component.callTimer();
+
+      verify(serviceMock.callDoSmthInteresting()).never();
     });
 
-    beforeEach(() => {
-      fixture = TestBed
-        .configureTestingModule({
-          declarations: [AsyncComponent],
-          providers: [
-            {provide: AsyncService, useValue: instance(asyncServiceMock)},
-            {provide: DependentService, useValue: instance(serviceMock)}
-          ]
-        })
-        .overrideTemplate(AsyncComponent, '')
-        .createComponent(AsyncComponent);
+    it('[fakeAsync]: should call callDoSmthInteresting method', fakeAsync(() => {
+      component.callTimer();
+      tick(AsyncService.TIME);
 
-      component = fixture.componentInstance;
+      verify(serviceMock.callDoSmthInteresting()).once();
+    }));
+  });
 
-      fixture.detectChanges();
-    });
-    // endregion
+  describe('debounce:', () => {
+    it('[sync]: should call callDoSmthInteresting method', () => {
+      component.callDebounce();
 
-    describe('timer:', () => {
-      it('[sync]: should call callDoSmthInteresting method', () => {
-        component.callTimer();
-
-        verify(serviceMock.callDoSmthInteresting()).once();
-      });
+      verify(serviceMock.callDoSmthInteresting()).never();
     });
 
-    describe('debounce:', () => {
-      it('[sync]: should call callDoSmthInteresting method', () => {
-        component.callDebounce();
+    it('[fakeAsync]: should call callDoSmthInteresting method', fakeAsync(() => {
+      component.callDebounce();
+      tick(AsyncService.TIME);
 
-        verify(serviceMock.callDoSmthInteresting()).once();
-      });
-    });
+      verify(serviceMock.callDoSmthInteresting()).once();
+    }));
   });
 });
